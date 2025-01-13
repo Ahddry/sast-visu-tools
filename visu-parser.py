337b208b780087533f99c4ce629ec2e66fc812aa
Ahddry <blair.adrien@outlook.com>
2025-01-13 09:42:41 +0100

    feat: update parser script

--- visu-parser.py
+++ visu-parser.py
@@ -43,59 +43,103 @@ def transform_sarif_to_report(sarif_report: Dict[str, Any]) -> Dict[str, Any]:
                 "extra": {
                     "engine_kind": "SARIF",
                     "fingerprint": result.get('partialFingerprints', {}).get('primaryLocationLineHash', result.get('fingerprints', {}).get("matchBasedId/v1", "")),
-                    "first_seen": None,
-                    "first_seen_report_number": 0,
-                    "fix": '',
-                    "is_ignored": False,
                     "lines": result['locations'][0]['physicalLocation']['region'].get('snippet', {}).get('text'),
-                    "message": result['message']['text'],
-                    "metadata": {
-                        "category": "security",
-                        "confidence": None,
-                        "cwe": cwe,
-                        "impact": None,
-                        "likelihood": rule.get('properties', {}).get('precision'),
-                        "owasp": owasp,
-                        "references": [rule.get('helpUri')] if rule.get('helpUri') else [],
-                        "semgrep.dev": None,
-                        "shortlink": rule.get('helpUri'),
-                        "source": rule.get('helpUri'),
-                        "subcategory": [],
-                        "technology": [technology] if technology else [],
-                        "vulnerability_class": []
-                    },
-                    "metavars": rule.get('properties'),
-                    "severity": rule['defaultConfiguration']['level'].replace("error", "High").replace("warning", "Medium").replace("note", "Low"),
-                    "validation_state": None
+                    "message": result['message']['text']
                 },
+                "first_seen": None,
+                "first_seen_report_number": 0,
+                "fix": '',
+                "is_ignored": False,
+                "metadata": {
+                    "confidence": None,
+                    "cwe": cwe,
+                    "impact": None,
+                    "likelihood": rule.get('properties', {}).get('precision'),
+                    "owasp": owasp,
+                    "references": [rule.get('helpUri')] if rule.get('helpUri') else [],
+                    "shortlink": rule.get('helpUri'),
+                    "source": rule.get('helpUri'),
+                    "language": [technology] if technology else [],
+                    "vulnerability_class": []
+                },
+                "name": rule.get('name', result['ruleId']),
                 "new": True,
                 "path": result['locations'][0]['physicalLocation']['artifactLocation']['uri'],
+                "severity": rule['defaultConfiguration']['level'].replace("error", "High").replace("warning", "Medium").replace("note", "Low"),
                 "start": {
                     "col": result['locations'][0]['physicalLocation']['region']['startColumn'],
                     "line": result['locations'][0]['physicalLocation']['region']['startLine'],
                     "offset": 0
-                }
+                },
+                "state": "To qualify"
             })
-
-    errors = [{
-        "code": notification['id'],
-        "level": notification['defaultConfiguration']['level'],
-        "message": notification['fullDescription']['text'],
-        "path": "",
-        "spans": [],
-        "type": []
-    } for notification in sarif_report['runs'][0]['tool']['driver'].get('notifications', [])]
 
     date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     report = {
         "date_time": date,
-        "errors": errors,
         "paths": {
-            "scanned": [result['path'] for result in results]
+            "scanned": list({result['path'] for result in results})
         },
         "results": results,
-        "version": sarif_report['version']
+        "version": sarif_report['version'],
+        "high": len([result for result in results if result['severity'] == "High" and not result['is_ignored']]),
+        "medium": len([result for result in results if result['severity'] == "Medium" and not result['is_ignored']]),
+        "low": len([result for result in results if result['severity'] == "Low" and not result['is_ignored']]),
+        "fixed": 0,
+        "ignored": len([result for result in results if result['is_ignored']]),
+        "fixedFindings": [],
+    }
+
+    return report
+
+def transform_json_to_report(json_report: Dict[str, Any]) -> Dict[str, Any]:
+    results = []
+    for result in json_report['results']:
+        results.append({
+            "check_id": result.get('check_id', ""),
+            "end": result['end'],
+            "extra": {
+                "engine_kind": result['extra']['engine_kind'],
+                "fingerprint": result['extra']['fingerprint'],
+                "lines": result['extra']['lines'],
+                "message": result['extra']['message'],
+            },
+            "first_seen": json_report.get('date_time'),
+            "first_seen_report_number": 0,
+            "is_ignored": result['extra'].get('is_ignored', False),
+            "metadata": {
+                "confidence": result['extra']['metadata'].get('confidence'),
+                "cwe": result['extra']['metadata'].get('cwe', []),
+                "impact": result['extra']['metadata'].get('impact'),
+                "likelihood": result['extra']['metadata'].get('likelihood'),
+                "owasp": result['extra']['metadata'].get('owasp', []),
+                "references": result['extra']['metadata'].get('references', []),
+                "shortlink": result['extra']['metadata'].get('shortlink', ""),
+                "source": result['extra']['metadata'].get('source', ""),
+                "language": result['extra']['metadata'].get('technology', []),
+                "vulnerability_class": result['extra']['metadata'].get('vulnerability_class', []),
+            },
+            "name": result.get('check_id', ""),
+            "new": True,
+            "path": result.get('path', ""),
+            "severity": result['extra'].get('severity', ""),
+            "start": result.get('start'),
+            "state": "To qualify",
+        })
+
+    report = {
+        "date_time": json_report['date_time'],
+        "paths": json_report['paths'],
+        "results": results,
+        "skipped_rules": json_report.get('skipped_rules', []),
+        "version": json_report['version'],
+        "high": len([result for result in results if result['severity'] == "High" and not result['is_ignored']]),
+        "medium": len([result for result in results if result['severity'] == "Medium" and not result['is_ignored']]),
+        "low": len([result for result in results if result['severity'] == "Low" and not result['is_ignored']]),
+        "fixed": 0,
+        "ignored": len([result for result in results if result['is_ignored']]),
+        "fixedFindings": [],
     }
 
     return report
@@ -113,15 +157,14 @@ def handle_new_json_report_import(file_path: str):
     with open(file_path, 'r') as file:
         content = file.read()
         json_data = json.loads(content)
-        report = {
+        jsonReport = {
             "date_time": json_data.get('date_time', datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
-            "errors": json_data['errors'],
-            "interfile_language_used": json_data.get('interfile_language_used', []),
             "paths": json_data['paths'],
             "results": json_data['results'],
             "skipped_rules": json_data.get('skipped_rules', []),
             "version": json_data['version'],
         }
+        report = transform_json_to_report(jsonReport)
         if dbConnectionOk:
             upload_report_to_mongodb(report)
         save_report(report)
@@ -147,11 +190,11 @@ def save_report(report: Dict[str, Any]):
 def process_new_report_results(report: Dict[str, Any], previous_report: Dict[str, Any]):
     print("Processing new report results")
     for result in report['results']:
-        result['extra']['severity'] = result['extra']['severity'].replace("ERROR", "High").replace("WARNING", "Medium").replace("INFO", "Low")
+        result['severity'] = result['severity'].replace("ERROR", "High").replace("WARNING", "Medium").replace("INFO", "Low")
 
         # Normalize CWE and OWASP tags
-        if isinstance(result['extra']['metadata']['owasp'], list):
-            result['extra']['metadata']['owasp'] = [owasp.replace("21-", "21 - ")
+        if isinstance(result['metadata']['owasp'], list):
+            result['metadata']['owasp'] = [owasp.replace("21-", "21 - ")
                                                     .replace("17-", "17 - ")
                                                     .replace("A1", "A01")
                                                     .replace("A2", "A02")
@@ -161,50 +204,60 @@ def process_new_report_results(report: Dict[str, Any], previous_report: Dict[str
                                                     .replace("A6", "A06")
                                                     .replace("A7", "A07")
                                                     .replace("A8", "A08")
-                                                    .replace("A9", "A09") for owasp in result['extra']['metadata']['owasp']]
+                                                    .replace("A9", "A09") for owasp in result['metadata']['owasp']]
         else:
-            result['extra']['metadata']['owasp'] = [result['extra']['metadata']['owasp']]
+            result['metadata']['owasp'] = [result['metadata']['owasp']]
 
-        # Normalize technology tags
-        if not result['extra']['metadata'].get('technology'):
-            technology = result['path'].split('.')[-1].replace("yml", "yaml")
-            if "key" in technology:
-                technology = "key"
-            elif "conf" in technology:
-                technology = "config file"
-            elif len(technology) > 30:
-                technology = "unknown"
-            result['extra']['metadata']['technology'] = [technology.upper()]
+        # Normalize language tags
+        if not result['metadata'].get('language'):
+            language = result['path'].split('.')[-1].replace("yml", "yaml")
+            if "key" in language:
+                language = "key"
+            elif "conf" in language:
+                language = "config file"
+            elif len(language) > 30:
+                language = "unknown"
+            result['metadata']['language'] = [language.upper()]
 
         # Add first seen date and report number
         if report.get('date_time'):
-            result['extra']['first_seen'] = report['date_time']
-        result['extra']['first_seen_report_number'] = report['report_number']
-
+            result['first_seen'] = report['date_time']
+        result['first_seen_report_number'] = report['report_number']
         # Check if the result is new or fixed
-        existing_result = next((prev_result for prev_result in previous_report['report']['results'] if compare_results(prev_result, result)), None)
+        existing_result = next((prev_result for prev_result in previous_report['results'] if compare_results(prev_result, result)), None)
         result['new'] = not existing_result
         if existing_result:
-            if existing_result['extra']['first_seen']:
-                result['extra']['first_seen'] = existing_result['extra']['first_seen']
-            result['extra']['first_seen_report_number'] = existing_result['extra'].get('first_seen_report_number', report['report_number'] - 1)
-            if existing_result['extra']['is_ignored']:
-                result['extra']['is_ignored'] = True
+            if existing_result['first_seen']:
+                result['first_seen'] = existing_result['first_seen']
+            result['first_seen_report_number'] = existing_result.get('first_seen_report_number', report['report_number'] - 1)
+            if existing_result['is_ignored']:
+                result['is_ignored'] = True
+            if existing_result['state']:
+                result['state'] = existing_result['state']
+            else:
+                result['state'] = "To qualify"
     print("Results processed, calculating new, fixed, and ignored findings")
     # Calculate new, fixed, and ignored findings
     previous_fixed_findings = previous_report.get('fixedFindings', [])
-    for finding in previous_fixed_findings:
+    new_fixed_findings = [result for result in previous_report['results'] if not next((new_result for new_result in report['results'] if compare_results(new_result, result)), None)]
+    for finding in new_fixed_findings:
         finding['new'] = False
-    new_fixed_findings = [result for result in previous_report['report']['results'] if not next((new_result for new_result in report['results'] if compare_results(new_result, result)), None)]
+        finding["state"] = "Fixed on " + report['date_time'] + " in report " + str(report['report_number'])
     fixed_findings = previous_fixed_findings + new_fixed_findings
     report_to_upload = {
-        "report": report,
-        "high": len([result for result in report['results'] if result['extra']['severity'] == "High" and not result['extra']['is_ignored']]),
-        "medium": len([result for result in report['results'] if result['extra']['severity'] == "Medium" and not result['extra']['is_ignored']]),
-        "low": len([result for result in report['results'] if result['extra']['severity'] == "Low" and not result['extra']['is_ignored']]),
+        "date_time": report['date_time'],
+        "paths": report['paths'],
+        "project_id": report['project_id'],
+        "results": report['results'],
+        "version": report['version'],
+        "high": len([result for result in report['results'] if result['severity'] == "High" and not result['is_ignored']]),
+        "medium": len([result for result in report['results'] if result['severity'] == "Medium" and not result['is_ignored']]),
+        "low": len([result for result in report['results'] if result['severity'] == "Low" and not result['is_ignored']]),
         "fixed": len(fixed_findings),
-        "ignored": len([result for result in report['results'] if result['extra']['is_ignored']]),
+        "ignored": len([result for result in report['results'] if result['is_ignored']]),
         "fixedFindings": fixed_findings,
+        "report_number": report['report_number'],
+        "version": report['version']
     }
 
     return report_to_upload
@@ -215,20 +268,23 @@ def upload_report_to_mongodb(report: Dict[str, Any]):
         mongodb_url = os.getenv("MONGODB_URL")
         mongodb_username = os.getenv("MONGODB_USERNAME")
         mongodb_password = os.getenv("MONGODB_PASSWORD")
+        #! Might need to add options based on the database type
+        ############################################################################################################
         mongoDB_conncection_string = f"mongodb://{mongodb_username}:{mongodb_password}@{mongodb_url}/?authSource=visu"
         client = MongoClient(mongoDB_conncection_string)
         collection = client["visu"]["reports"]
         report['project_id'] = int(project_id)
-        report['report_number'] = collection.count_documents({"report.project_id": int(project_id)})
+        report['report_number'] = collection.count_documents({"project_id": int(project_id)})
         try:
-            previous_report = collection.find_one({"report.project_id": int(project_id), "report.report_number": report['report_number'] - 1}, {"_id": 0})
-            if previous_report["report"]["report_number"] != report['report_number'] - 1:
+            previous_report = collection.find_one({"project_id": int(project_id), "report_number": report['report_number'] - 1}, {"_id": 0})
+            if previous_report["report_number"] != report['report_number'] - 1:
                 raise Exception("Previous report not found")
         except Exception as e:
             print(f"Failed to get previous report from MongoDB: {e}")
             sys.exit(1)
         report_to_upload = process_new_report_results(report, previous_report)
-        # collection.insert_one(report_to_upload)
+        # NOTE: For debugging purposes, comment the next line to avoid uploading the report to MongoDB
+        collection.insert_one(report_to_upload)
         print("Report uploaded to MongoDB")
     except Exception as e:
         print(f"Failed to upload report to MongoDB: {e}")
